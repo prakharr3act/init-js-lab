c7fbc1e8efe5b7a3bd5a659eeb74e8a5b747d452
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-08 16:11:55 +0530

    add fnc to sort

---
+++ gradeSorter.js
@@ -0,0 +1,11 @@
+function gradeSorter(grades){
+    if (grades >= 90){
+        console.log("A")
+    }
+    else if (grades >= 80){
+        console.log("B")
+    }
+    
+}
+grades = prompt("Enter your grade")
+gradeSorter(grades)
