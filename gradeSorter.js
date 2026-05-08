ab91c0d3a8eb5557dd175a1fe1841e21e9115549
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-08 16:14:37 +0530

    fix fnc

--- gradeSorter.js
+++ gradeSorter.js
@@ -5,7 +5,18 @@ function gradeSorter(grades){
     else if (grades >= 80){
         console.log("B")
     }
-    
+    else if (grades >= 70){
+        console.log("C")
+    }
+     else if (grades >= 60){
+        console.log("D")
+    }
+    else if (grades >= 50){
+        console.log("F")
+    }
+     else {
+        console.log("Invalid grade")
+    }
 }
 grades = prompt("Enter your grade")
 gradeSorter(grades)
