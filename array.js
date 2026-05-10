6337a39fd9c7ce157339de934c0fef0d8ae67ca9
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-10 18:28:25 +0530

    add sort fnc

--- array.js
+++ array.js
@@ -42,3 +42,19 @@ console.log(arr)
 let marks = [21,42,31,0,1,26,32,3,41,89,98,84,1,2,11,25,69,36,51,78,81,21,3,2,14,15,62,15,]
 marks.sort()
 console.log(marks)
+
+
+// ascending order
+let ao = marks.sort(function(a,b){
+  return a-b;
+});
+console.log(ao)
+
+
+//descending order
+
+let Do = marks.sort(function(a,b){
+  return b-a;
+});
+console.log(Do);
+
