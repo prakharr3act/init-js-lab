0989a45f33428ef2d3d40916b9f3e6f2508a9672
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-10 18:38:02 +0530

    exit console

--- advnArray.js
+++ advnArray.js
@@ -3,3 +3,13 @@ let marks = [21,42,31,0,1,26,32,3,41,89,98,84,1,2,11,25,69,36,51,78,81,21,3,2,14
 marks.forEach(function(value){
   console.log(value)
 })
+
+// use map only when you want to make a new array
+let newArr = marks.map(function(value){
+  return 1;
+})
+
+console.log(newArr)
+
+//filter
+
