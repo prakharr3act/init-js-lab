f3df385d2cad8c3ded136069116f458afec73066
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-11 10:41:54 +0530

    remove

--- sid-js02/dom.js
+++
@@ -1,5 +0,0 @@
-let a = document.querySelector('#link');
-a.addEventListener('click', function(event) {
-  alert("You have been logged out.");
-  window.location.href = "https://www.google.com";
-});
