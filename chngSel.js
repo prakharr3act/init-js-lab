b69705bd0e6daf8e6f816e18363b43d606610e82
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-12 10:11:51 +0530

    add chngSel.js

---
+++ chngSel.js
@@ -0,0 +1,8 @@
+// change event
+// when input or textarea shows any change
+
+let select = document.querySelector("select");
+let h3 =  document.querySelector("h3");
+select.addEventListener("change",function(event){
+h3.textContent = `${event.target.value} is selected`;
+})
--- eventHandling.js
+++ eventHandling.js
@@ -5,11 +5,3 @@
 //   }
 // })
 
-// change event
-// when input or textarea shows any change
-
-let select = document.querySelector("select");
-let h3 =  document.querySelector("h3");
-select.addEventListener("change",function(event){
-h3.textContent = `${event.target.value} is selected`;
-})
