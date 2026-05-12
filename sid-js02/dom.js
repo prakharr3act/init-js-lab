143fd52270da53381c69f5086725a8ed70da933e
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-12 08:32:10 +0530

    exit dom

---
+++ sid-js02/dom.js
@@ -0,0 +1,6 @@
+// DOM Manipulation Functions
+// This file contains functions for manipulating the Document Object Model (DOM)
+
+// classList.toggle() - Toggles a class on an element
+// remove() - Removes an element from the DOM
+// appendChild() - Adds a new child element to a parent element
