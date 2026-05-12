c100acefb3f799170d084f0bcfe2a90149bf3faf
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-12 10:07:46 +0530

    add event.target

--- eventHandling.js
+++ eventHandling.js
@@ -1,6 +1,15 @@
-let inp = document.querySelector("input");
-inp.addEventListener("input",function(event){
-  if(event.data !== null){
-    console.log(event.data);
-  }
+// let inp = document.querySelector("input");
+// inp.addEventListener("input",function(event){
+//   if(event.data !== null){
+//     console.log(event.data);
+//   }
+// })
+
+// change event
+// when input or textarea shows any change
+
+let select = document.querySelector("select");
+let h3 =  document.querySelector("h3");
+select.addEventListener("change",function(event){
+h3.textContent = `${event.target.value} is selected`;
 })
