04e4d556cb01850428923172ced747ca248021b0
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-12 19:52:34 +0530

    exit prc prj

--- sid-js02/prc-prj2/uplFile/uf.js
+++ sid-js02/prc-prj2/uplFile/uf.js
@@ -1,6 +1,6 @@
 let inp = document.querySelector("input");
 let p = document.querySelector("p");
-inp.style.display = "none";
+// inp.style.display = "none";
 
 p.addEventListener("click", function(){
   inp.click();
