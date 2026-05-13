282cf2950ba39ded9ddeb3259da113a0d27dbf5e
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-13 07:16:40 +0530

    exit prc

--- sid-js02/prc-prj2/usrCrd/usrcd.js
+++ sid-js02/prc-prj2/usrCrd/usrcd.js
@@ -1,5 +1,5 @@
 let form = document.querySelector('form');
-let inputs = document.querySelector("input");
+let inputs = document.querySelectorAll("input");
 let name = document.getElementById("name");
 let age = document.getElementById("age");
 let email = document.getElementById("email");
@@ -8,7 +8,18 @@ let submitBtn = document.getElementById("submitBtn");
 
 form.addEventListener("submit",function(event){
   event.preventDefault();
+
   let div = document.createElement("div");
   div.setAttribute("class","container")
+  div.innerHTML = `
+   <img src="${profilePic.value}" alt="Profile Picture" id="profilePic">
+    <p id="name">${name.value}</p>
+    <p id="age">${age.value}</p>
+    <p id="email">${email.value}</p>
+  `
   document.querySelector("body").append(div);
-})
+  name.value = "";
+  age.value = "";
+  profilePic.value = "";
+  email.value = "";
+}) 
