946ab864503b5a34320e8356779c07633511a57c
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-13 07:01:23 +0530

    fix id

--- sid-js02/prc-prj2/usrCrd/usrcd.js
+++ sid-js02/prc-prj2/usrCrd/usrcd.js
@@ -1,4 +1,14 @@
 let form = document.querySelector('form');
+let inputs = document.querySelector("input");
+let name = document.getElementById("name");
+let age = document.getElementById("age");
+let email = document.getElementById("email");
+let profilePic = document.getElementById("profilePic");
+let submitBtn = document.getElementById("submitBtn");
+
 form.addEventListener("submit",function(event){
   event.preventDefault();
+  let div = document.createElement("div");
+  div.setAttribute("class","container")
+  document.querySelector("body").append(div);
 })
