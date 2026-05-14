e485733a9e3c05321cad2a197f8f5ea5b7325140
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-14 19:35:16 +0530

    fix

--- sid-js02/timersInt.js
+++ sid-js02/timersInt.js
@@ -5,8 +5,8 @@ let inter = setInterval(function(){
       console.log(` You will be logged out in ${count} seconds...`)
   }
  else{
- clearInterval(inter, function(){
- document.querySelector("div").style.display = "initial"
- })
+ clearInterval(inter);
+    console.log("You have been logged out");
+    document.querySelector("div").style.display = "block";
  }
 },1000)
