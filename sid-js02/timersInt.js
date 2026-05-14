ad3100475246fd1ee75f7cf65b8106066d62cab6
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-14 19:38:41 +0530

    fix theme

--- sid-js02/timersInt.js
+++ sid-js02/timersInt.js
@@ -1,12 +1,12 @@
-let count = 10;
-let inter = setInterval(function(){
-  if(count >= 1 ){
-    count--;
-      console.log(` You will be logged out in ${count} seconds...`)
-  }
- else{
- clearInterval(inter);
-    console.log("You have been logged out");
-    document.querySelector("div").style.display = "block";
- }
-},1000)
+// let count = 10;
+// let inter = setInterval(function(){
+//   if(count >= 1 ){
+//     count--;
+//       console.log(` You will be logged out in ${count} seconds...`)
+//   }
+//  else{
+//  clearInterval(inter);
+//     console.log("You have been logged out");
+//     document.querySelector("div").style.display = "block";
+//  }
+// },1000)
