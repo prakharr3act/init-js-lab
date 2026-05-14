611eb83bb650fbfebd46ba41f8096010305ec6a6
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-14 19:31:00 +0530

    add clearInt

--- sid-js02/timersInt.js
+++ sid-js02/timersInt.js
@@ -2,9 +2,11 @@ let count = 10;
 let inter = setInterval(function(){
   if(count >= 1 ){
     count--;
-      console.log(count)
+      console.log(` You will be logged out in ${count} seconds...`)
   }
  else{
- clearInterval(inter)
+ clearInterval(inter, function(){
+ document.querySelector("div").style.display = "initial"
+ })
  }
 },1000)
