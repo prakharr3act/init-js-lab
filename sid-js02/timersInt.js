dc2460f1d38a742152cf66389b2ff64439d974de
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-14 19:47:09 +0530

    add reload

--- sid-js02/timersInt.js
+++ sid-js02/timersInt.js
@@ -1,12 +1,24 @@
-// let count = 10;
-// let inter = setInterval(function(){
-//   if(count >= 1 ){
-//     count--;
-//       console.log(` You will be logged out in ${count} seconds...`)
-//   }
-//  else{
-//  clearInterval(inter);
-//     console.log("You have been logged out");
-//     document.querySelector("div").style.display = "block";
-//  }
-// },1000)
+let count = 10;
+let button = document.querySelector("button");
+button.addEventListener("click",function(){
+      document.querySelector("div").style.display = "none";
+    window.location.reload();
+
+})
+let inter = setInterval(function(){
+  if(count >= 1 ){
+    count--;
+      console.log(` You will be logged out in ${count} seconds...`)
+  }
+ else{
+ clearInterval(inter);
+    console.log("You have been logged out");
+    document.querySelector("div").style.display = "block";
+ }
+},1000)
+
+
+let div = document.querySelector("div");
+div.addEventListener("click",function(events){
+  
+})
