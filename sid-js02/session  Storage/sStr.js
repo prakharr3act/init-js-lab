50408e8af9d1c3496f8ed169d15175b8c5e9b5d1
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-17 08:46:46 +0530

    exit sStr

---
+++ sid-js02/session  Storage/sStr.js
@@ -0,0 +1,17 @@
+//sessionStorage
+
+//store
+// sessionStorage.setItem("name", "Prakhar Patel");
+
+//get or fetch data
+// let value = sessionStorage.getItem("name");
+// console.log(value);
+
+//remove
+// sessionStorage.removeItem("theme")
+
+
+//update
+
+// sessionStorage.setItem("name", "Prakharr._Patel");
+
