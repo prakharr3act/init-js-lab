e7d430cdd7dd879035e4b99b76f63f9d54e9df8d
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-17 08:42:31 +0530

    exit lcl

---
+++ sid-js02/local storage/lclStrg.js
@@ -0,0 +1,17 @@
+//localStorage
+
+//store
+// localStorage.setItem("name", "Prakhar Patel");
+
+//get or fetch data
+// let value = localStorage.getItem("name");
+// console.log(value);
+
+//remove
+// localStorage.removeItem("theme")
+
+
+//update
+
+// localStorage.setItem("name", "Prakharr._Patel");
+
